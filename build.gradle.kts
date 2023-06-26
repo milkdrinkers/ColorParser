089ce1ec1ab36ec08d707b736fe81ef7ca1b1436
darksaid98 <darksaid9889@gmail.com>
2023-06-26 03:55:11 +0200

    chore: build changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,13 @@ plugins {
     `java-library`
     `maven-publish`
 
-    id("com.github.johnrengelman.shadow") version "8.1.1" // Shades and relocates dependencies, See https://imperceptiblethoughts.com/shadow/introduction/
+//    id("com.github.johnrengelman.shadow") version "8.1.1" // Shades and relocates dependencies, See https://imperceptiblethoughts.com/shadow/introduction/
 }
 
 group = "com.github.milkdrinkers"
 version = "1.0.2"
 description = ""
+
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17)) // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
@@ -24,9 +25,7 @@ repositories {
         content { includeGroup("me.clip") }
     }
 
-    maven("https://jitpack.io/") {
-        content {}
-    }
+    maven("https://jitpack.io/")
 }
 
 dependencies {
@@ -35,31 +34,27 @@ dependencies {
 }
 
 tasks {
-    build {
+    /*build {
         dependsOn(shadowJar)
-    }
+    }*/
 
     compileJava {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-
-        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
-        // See https://openjdk.java.net/jeps/247 for more information.
-        options.release.set(17)
     }
 
-    shadowJar {
+    /*shadowJar {
         archiveBaseName.set(project.name)
         archiveClassifier.set("")
 
         // Shadow classes
         // helper function to relocate a package into our package
         fun reloc(originPkg: String, targetPkg: String) = relocate(originPkg, "${project.group}.colorparser.${targetPkg}")
-    }
+    }*/
 }
 
 publishing {
     publications {
-        create<MavenPublication>("colorparser") {
+        create<MavenPublication>("maven") {
             groupId = "${project.group}"
             artifactId = "colorparser"
             version = "${project.version}"
