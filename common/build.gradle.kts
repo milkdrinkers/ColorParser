8e249ce9227d1bd465fd0c1174549a88d49a5d43
darksaid98 <darksaid9889@gmail.com>
2025-02-23 17:57:11 +0100

    build: fix wrong maven name

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -19,10 +19,7 @@ deployer {
             url = "https://github.com/milkdrinkers/ColorParser"
         }
 
-        license({
-            name = "GNU General Public License Version 3"
-            url = "https://opensource.org/license/gpl-3-0/"
-        })
+        license("GNU General Public License Version 3", "https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
 
         developer({
             name.set("darksaid98")
